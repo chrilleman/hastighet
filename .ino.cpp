36de29c47b1eb7a0f3d2ee6f44a7a018c664baf9
chrilleman <christer.hoerlin@gmail.com>
2016-11-27 08:36:54 +0100

    adding index to .ino.cpp

---
+++ .ino.cpp
@@ -0,0 +1,15 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2016-11-27 08:33:12
+
+#include "Arduino.h"
+#include "Arduino.h"
+void setup() ;
+void loop() ;
+
+#include "hastighet.ino"
+
+
+#endif

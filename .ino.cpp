cbc044ca664a14dc3364ef018810e279d6d06944
chrilleman <christer.hoerlin@gmail.com>
2016-11-27 08:24:01 +0100

    testar commit igen

--- .ino.cpp
+++ .ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-11-27 08:04:12
+//This file has been generated on 2016-11-27 08:22:54
 
 #include "Arduino.h"
 #include "Arduino.h"
--- hastighet.ino
+++ hastighet.ino
@@ -1,5 +1,8 @@
 #include "Arduino.h"
 //The setup function is called once at startup of the sketch
+
+#define LED 13
+
 void setup()
 {
 // Add your initialization code here
